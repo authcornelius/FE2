d168279ae487729c1749bce97afb63c33471f4d6
authcornelius <corneliusedos@gmail.com>
2024-07-02 06:34:45 +0100

    test

--- script.js
+++ script.js
@@ -4,8 +4,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function updateTime() {
         const now = new Date();
-        currentTimeElement.textContent = now.toUTCString().split(' ')[4];
-        currentDayElement.textContent = now.toLocaleDateString('en-US', { weekday: 'long' });
+        const options = { timeZone: 'Africa/Lagos', hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false };
+        const timeFormatter = new Intl.DateTimeFormat('en-US', options);
+        const dayFormatter = new Intl.DateTimeFormat('en-US', { timeZone: 'Africa/Lagos', weekday: 'long' });
+
+        currentTimeElement.textContent = timeFormatter.format(now);
+        currentDayElement.textContent = dayFormatter.format(now);
     }
 
     updateTime();
